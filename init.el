f414065140eb6495d2a5607f97160e87178a9254
himonoinu <1418730576@qq.com>
2022-07-02 13:41:32 +0800

    7.2

--- init.el
+++ init.el
@@ -28,22 +28,62 @@
   :ensure t
   :init (global-company-mode)
   :config
-  (setq company-minimum-prefix-length 2) ;
+  (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
+  (setq company-tooltip-align-annotations t)
   (setq company-idle-delay 0.0)
-  (setq company-show-numbers t) 
+  (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
   (setq company-selection-wrap-around t)
-  (setq company-transformers '(company-sort-by-occurrence))) ;
+  (setq company-transformers '(company-sort-by-occurrence))) ; 
 
-(require 'company-clang)
-(add-hook 'after-init-hook 'global-company-mode)
-(add-hook 'c++-mode-hook 'company-mode)
+(package-install 'yasnippet)
+(use-package yasnippet
+  :ensure t
+  :init
+  ;(yas-reload-all)
+  :hook
+  (prog-mode . yas-minor-mode)
+  :config
+  ;; add company-yasnippet to company-backends
+  (defun company-mode/backend-with-yas (backend)
+    (if (and (listp backend) (member 'company-yasnippet backend))
+	backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+  ;; unbind <TAB> completion
+  (define-key yas-minor-mode-map [(tab)]        nil)
+  (define-key yas-minor-mode-map (kbd "TAB")    nil)
+  (define-key yas-minor-mode-map (kbd "<tab>")  nil)
+  :bind
+  (:map yas-minor-mode-map ("S-<tab>" . yas-expand)))
+
+(package-install 'yasnippet-snippets)
+(use-package yasnippet-snippets
+  :ensure 
+  :after yasnippet)
+
+
+(use-package lsp-mode
+  :ensure t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l"
+	lsp-file-watch-threshold 500)
+  :hook 
+  (lsp-mode . lsp-enable-which-key-integration) ; which-key integration
+  :commands (lsp lsp-deferred)
+  :config
+    (setq lsp-completion-provider :none) ;; 阻止 lsp 重新设置 company-backend 而覆盖我们 yasnippet 的设置
+    (setq lsp-headerline-breadcrumb-enable t))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(vertico company use-package orderless)))
+ '(package-selected-packages
+   '(lsp-mode yasnippet-snippets yasnippet vertico company use-package orderless)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
